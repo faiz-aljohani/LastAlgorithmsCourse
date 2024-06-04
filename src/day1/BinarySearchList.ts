4ddf1456bc08e399636157139b54fa35e98b3149
nufox227 <nufox227@gmail.com>
2024-06-04 21:21:32 +0300

    feat: Implement binary search algorithm in BinarySearchList.ts and bubble sort algorithm in BubbleSort.ts, and optimize linear search algorithm in LinearSearchList.ts

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    while (lo < hi) {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    }
+    return false;
 }
+
+// for (let i = haystack.length / 2; i < haystack.length; i / 2) {
+//   if (haystack[i] === needle) return true;
+//   else if (haystack[i] < needle) i *= 3;
+// }
+// return false;
