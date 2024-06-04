6e242d99c6000890bf4c619987a50b820113336e
nufox227 <nufox227@gmail.com>
2024-06-05 01:20:48 +0300

    feat: implmented queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
-
-}
+        let itemNode: Node<T> = { value: item };
+        if (this.head === undefined) {
+            this.head = itemNode;
+        }
+        if (this.length >= 1 && this.tail) {
+            this.tail.next = itemNode;
+        }
+        this.tail = itemNode;
+        this.length++;
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        let item = this.head.value;
+        if (this.length > 1) {
+            this.head = this.head.next;
+            this.length--;
+        } else if (this.length == 1) {
+            let item = this.head.value;
+            this.head = this.tail = undefined;
+            this.length--;
+        }
+        return item;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
